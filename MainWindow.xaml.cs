5254e4c3b52b5f295223e655db2e3106b83f09e2
sahandak.milani <59655828+SahandMilani@users.noreply.github.com>
2024-08-08 22:30:16 +0330

    project created, simple buttons  and grid added.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfCalculator;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var button = (Button)sender;
+        var operandString = button.Tag.ToString();
+        int operand = int.Parse(operandString);
+        MessageBox.Show(operand.ToString());
+    }
+}
